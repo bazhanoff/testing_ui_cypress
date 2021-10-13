cf99f30195618eda9f62b9b3e038d1429bc42998
Vlad Bazhanov <VOBazhanov@sberbank.ru>
2021-10-13 11:29:01 +0300

    Добавляет бутстрап заданий для тестирования

---
+++ cypress/integration/auth/error-auth.e2e.test.js
@@ -0,0 +1,34 @@
+/// <reference types="Cypress" />
+import { status } from '../../fixtures/authentication/authentication-error-status';
+
+context('Auth', () => {
+    it('Покажи Алерт, если авторизовался неверно с кодом 403', function() {
+        cy.viewport(300, 600);
+        cy.server()
+        cy.visit('/')
+
+        // 1 Поправить status, взять из import { status }
+        cy.route({
+            method: 'POST',
+            url: '**/api/authentication',
+            response: 'fixture:authentication/authentication-error.json',
+            status: 403,
+        }).as('auth-xhr')
+
+        cy.findByPlaceholderText('Your username').type('smagni@workwave.com');
+        cy.findByPlaceholderText('Your password').type('mysupersecretpassword');
+        cy.findByRole('button', { name: 'Login' }).click();
+        
+        // 2 Исправить тест
+        cy.findByText('An error occured, please retrydasdada').should('be.visible');
+    })
+
+
+    it('Покажи Алерт, если авторизовался неверно с кодом 401', function() {
+        cy.viewport(300, 600);
+        cy.server()
+        cy.visit('/')
+
+        // 3 Дописать тесты
+    })
+})
---
+++ cypress/integration/auth/sucess-auth.e2e.test.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+context("Auth", () => {
+    it("Должен работать с успешной авторизацией", () => {
+        const username = "smagni@workwave.com";
+        const password = "mysupersecretpassword";
+
+        // 4. Дописать успешную авторизацию
+  });
+
+});
