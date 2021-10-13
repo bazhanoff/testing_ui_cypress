d76835c732df0c0ff5d5defa23612498c25ea83f
Kris <bloodlector@mail.ru>
2021-10-13 18:31:21 +0300

    401, 403 и 200 статусы прописал

--- cypress/integration/auth/sucess-auth.e2e.test.js
+++ cypress/integration/auth/sucess-auth.e2e.test.js
@@ -1,10 +1,28 @@
 /// <reference types="Cypress" />
 context("Auth", () => {
-    it("Должен работать с успешной авторизацией", () => {
-        const username = "smagni@workwave.com";
-        const password = "mysupersecretpassword";
+  it("Должен работать с успешной авторизацией", () => {
+    const username = "smagni@workwave.com";
+    const password = "mysupersecretpassword";
 
-        // 4. Дописать успешную авторизацию
+    // 4. Дописать успешную авторизацию
+    cy.viewport(400, 600);
+    cy.server()
+    cy.visit('/')
+
+
+    cy.route({
+      method: 'POST',
+      url: '**/api/authentication',
+      response: 'fixture:authentication/authentication-error.json',
+      status: status[2],
+    }).as('auth-xhr')
+
+    cy.findByPlaceholderText('Your username').type('smagni@workwave.com');
+    cy.findByPlaceholderText('Your password').type('mysupersecretpassword');
+    cy.findByRole('button', { name: 'Login' }).click();
+
+    // 2 Исправить тест
+    cy.findByText('Welcome back!').should('be.visible')
   });
 
 });
